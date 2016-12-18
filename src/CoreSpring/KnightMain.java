79e3406bda7533fba0a2efc10529cc4d7335a8ac
Zihan.Cong <zihan.cong@gmail.com>
2016-12-18 21:45:57 +0800

    KnightMain加载包含Knight的Spring上下文

---
+++ src/CoreSpring/KnightMain.java
@@ -0,0 +1,15 @@
+package CoreSpring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by root on 2016/12/18.
+ */
+public class KnightMain {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("CoreSpring/XMLConfigs/knights.xml");
+        Knight knight = context.getBean(Knight.class);
+        knight.embarkOnQuest();
+        context.close();
+    }
+}
