137c26612de71d2408ab0ec35dabffb5046a0aeb
Zihan.Cong <zihan.cong@gmail.com>
2016-12-18 22:24:05 +0800

    吟游诗人是中世纪的音乐记录器

---
+++ src/CoreSpring/Ministrel.java
@@ -0,0 +1,24 @@
+package CoreSpring;
+
+import java.io.PrintStream;
+
+/**
+ * Created by root on 2016/12/18.
+ */
+public class Ministrel {
+    private PrintStream stream;
+
+    public Ministrel(PrintStream stream) {
+        this.stream = stream;
+    }
+
+    //探险之前调用
+    public void singBeforeQuest(){
+        stream.println("Fa la la,the knight is so brave!");
+    }
+
+    //探险之后调用
+    public void singAfterQuest(){
+        stream.println("Tee hee hee, the brave knight did embark on a quest");
+    }
+}
