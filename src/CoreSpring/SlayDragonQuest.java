7f4a9d2e703ef2883eaf7f2015d6575fa5fa18b3
Zihan.Cong <zihan.cong@gmail.com>
2016-12-18 16:34:39 +0800

    SlayDragonQuest是要注入到BraveKnight中的Quest实现

---
+++ src/CoreSpring/SlayDragonQuest.java
@@ -0,0 +1,20 @@
+package CoreSpring;
+
+import java.io.PrintStream;
+
+/**
+ * Created by congzihan on 16/12/18.
+ */
+public class SlayDragonQuest implements Quest {
+    private PrintStream stream;
+
+    public SlayDragonQuest(PrintStream stream) {
+        this.stream = stream;
+    }
+
+
+    @Override
+    public void embark() {
+        stream.println("Embarking on quest to slay the dragon");
+    }
+}
