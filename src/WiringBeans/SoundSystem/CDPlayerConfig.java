ed37eb8b264bb66820aadd792d869aa399269396
Zihan.Cong <zihan.cong@gmail.com>
2016-12-23 00:51:50 +0800

    通过JavaConfig装配Bean

--- src/WiringBeans/SoundSystem/CDPlayerConfig.java
+++ src/WiringBeans/SoundSystem/CDPlayerConfig.java
@@ -1,12 +1,20 @@
 package WiringBeans.SoundSystem;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by congzihan on 16/12/22.
  */
 @Configuration
-@ComponentScan(basePackages = {"WiringBeans.SoundSystem"})
 public class CDPlayerConfig {
+    @Bean
+    public CompactDisc sgtPeppers(){
+        return new SgtPeppers();
+    }
+
+    @Bean
+    public CDPlayer cdPlayer(CompactDisc compactDisc){
+        return new CDPlayer(compactDisc);
+    }
 }
