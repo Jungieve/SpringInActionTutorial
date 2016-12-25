f361314a5a2153e7d9fe7bdb6d9f63cb78c3c069
Zihan.Cong <zihan.cong@gmail.com>
2016-12-25 21:48:27 +0800

    标示首选的bean

--- src/CoreSpring/AdvancedWiring/Dessert/IceCream.java
+++ src/CoreSpring/AdvancedWiring/Dessert/IceCream.java
@@ -1,10 +1,12 @@
 package CoreSpring.AdvancedWiring.Dessert;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by congzihan on 16/12/25.
  */
 @Component
+@Primary
 public class IceCream implements Dessert {
 }
