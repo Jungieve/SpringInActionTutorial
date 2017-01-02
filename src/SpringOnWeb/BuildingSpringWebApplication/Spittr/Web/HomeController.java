dd883d17086da92974dccf27927ca0a6f1cbfa37
Zihan.Cong <zihan.cong@gmail.com>
2017-01-03 00:42:45 +0800

    增加homeController

---
+++ src/SpringOnWeb/BuildingSpringWebApplication/Spittr/Web/HomeController.java
@@ -0,0 +1,17 @@
+package SpringOnWeb.BuildingSpringWebApplication.Spittr.Web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by congzihan on 17/1/1.
+ */
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        System.out.println(1);
+        return "home";
+    }
+}
